f017cda0b38e691df5648643668502bb40c6fcb4
Jed Opaon <jedgamerdev@gmail.com>
2025-02-25 00:52:15 +0800

    removed backend link

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,8 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use(
-  "https://exercise-tracker-626p.onrender.com/api/workouts",
-  workoutRoutes
-);
-app.use("https://exercise-tracker-626p.onrender.com/api/user", userRoutes);
+app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 // connect to db
 mongoose
